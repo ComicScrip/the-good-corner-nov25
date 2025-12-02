9ee044554d142b7da99622cf5dd0121159780698
Pierre Genthon <genthon.pierre@gmail.com>
2025-12-02 11:18:04 +0100

    init front

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import dynamic from "next/dynamic";
 
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-// Disabling SSR
-export default dynamic(() => Promise.resolve(App), { ssr: false });
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,18 +1,3 @@
-import Head from "next/head";
-import Header from "@/components/Header";
-import RecentAds from "@/components/RecentAds";
-
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>The good corner</title>
-        <meta name="description" content="browse and publish ads" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <RecentAds />
-    </>
-  );
+	return <div>hello</div>;
 }
