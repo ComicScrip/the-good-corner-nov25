1830231fd00ee88a99093416f029308f39655df4
Pierre Genthon <genthon.pierre@gmail.com>
2025-12-03 15:44:53 +0100

    init frontend, add some styles, create header and recent ads components

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<header className="p-4 border-b border-gray-400">
+			<h1 className="text-orange-600 text-2xl font-bold">The good corner</h1>
+		</header>
+	);
+}
