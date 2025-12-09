53e0d345bd7276b8d56ce9c5f3ceffc6099beb84
Pierre Genthon <genthon.pierre@gmail.com>
2025-12-09 20:55:01 +0100

    mise en place graphql front avec codegen

---
+++ frontend/codegen.ts
@@ -0,0 +1,22 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:4001",
+  documents: ["**/*.{gql,graphql}"],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    "./src/graphql/generated/schema.ts": {
+      // see https://github.com/dotansimha/graphql-code-generator/issues/5073
+      plugins: ["typescript", "typescript-operations", "typescript-react-apollo", { "add": { content: '// @ts-nocheck' } }],
+
+    },
+  },
+  // see https://github.com/dotansimha/graphql-code-generator-community/issues/1216
+  config: {
+    apolloReactCommonImportFrom: "@apollo/client/react",
+    apolloReactHooksImportFrom: "@apollo/client/react",
+  }
+};
+
+export default config;
--- frontend/src/components/AdCard.tsx
+++ frontend/src/components/AdCard.tsx
@@ -3,32 +3,32 @@ import type { Ad } from "@/types";
 import FavoriteButton from "./FavoriteButton";
 
 interface AdCardProps {
-	ad: Ad;
+  ad: Ad;
 }
 
 export default function AdCard({
-	ad: { title, price, pictureUrl, id },
+  ad: { id, title, pictureUrl, price },
 }: AdCardProps) {
-	return (
-		<Link
-			href={`http://localhost:3000/ads/${id}`}
-			className="w-[400px] cursor-pointer"
-		>
-			<div className="relative shadow-md border rounded-lg p-4 bg-white mr-3 mb-3">
-				{/** biome-ignore lint/performance/noImgElement: images come form unknown domains */}
-				<img
-					className="h-[200px] w-full object-cover rounded-md"
-					src={pictureUrl}
-					alt={title}
-				/>
-				<div className="flex justify-between pt-6">
-					<div className="ad-card-title">{title}</div>
-					<div className="ad-card-price">{price} €</div>
-				</div>
-				<div className="absolute top-6 right-6">
-					<FavoriteButton />
-				</div>
-			</div>
-		</Link>
-	);
+  return (
+    <Link
+      href={`http://localhost:3000/ads/${id}`}
+      className="w-[400px] cursor-pointer"
+    >
+      <div className="relative shadow-md border rounded-lg p-4 bg-white mr-3 mb-3">
+        {/** biome-ignore lint/performance/noImgElement: images come form unknown domains */}
+        <img
+          className="h-[200px] w-full object-cover rounded-md"
+          src={pictureUrl}
+          alt={title}
+        />
+        <div className="flex justify-between pt-6">
+          <div className="ad-card-title">{title}</div>
+          <div className="ad-card-price">{price} €</div>
+        </div>
+        <div className="absolute top-6 right-6">
+          <FavoriteButton />
+        </div>
+      </div>
+    </Link>
+  );
 }
