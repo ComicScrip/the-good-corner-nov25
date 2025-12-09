e438d63b5d162dcca2b16a216af6ad647dcc00a3
Pierre Genthon <genthon.pierre@gmail.com>
2025-12-09 11:19:38 +0100

    graphql back init

--- backend/src/entities/Category.ts
+++ backend/src/entities/Category.ts
@@ -1,5 +1,4 @@
 import { Length } from "class-validator";
-import { Field, ID, ObjectType } from "type-graphql";
 import {
   BaseEntity,
   Column,
@@ -9,19 +8,15 @@ import {
 } from "typeorm";
 import { Ad } from "./Ad";
 
-@ObjectType()
 @Entity()
 export class Category extends BaseEntity {
-  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Field()
   @Column({ length: 100 })
   @Length(2, 100)
   name: string;
 
-  @Field(() => [Ad])
   @OneToMany(
     () => Ad,
     (ad) => ad.category,
