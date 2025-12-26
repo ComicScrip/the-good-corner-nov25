5e4c3979949f5cf19fad212cc75edfde676c73aa
Pierre Genthon <genthon.pierre@gmail.com>
2025-12-26 18:50:44 +0100

    logout refreshes profile + menu burger on mobile

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,14 +3,11 @@ import { ApolloProvider } from "@apollo/client/react";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import client from "@/graphql/client";
-import { AuthProvider } from "@/contexts/AuthContext";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 }
