97f80ad26db758d18d68d990eeca0c21b7458b76
Pierre Genthon <genthon.pierre@gmail.com>
2025-12-26 19:03:36 +0100

    improved styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -149,7 +149,7 @@ export default function Header() {
           <SearchInput inputClassName="w-sm" />
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
           {!loading && (
             <>
               {user ? (
