5979035379fcfbe26918eed50194769f32534ae0
Pierre Genthon <genthon.pierre@gmail.com>
2026-01-31 17:19:44 +0100

    working

--- run-prod.sh
+++ run-prod.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Ensure a consistent DEPLOY_ENV value for both the shell and compose
 export DEPLOY_ENV=${DEPLOY_ENV:-staging}
 
 docker compose -p "$DEPLOY_ENV" -f docker-compose.prod.yml --env-file .env.production down && \
