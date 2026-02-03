c8dcc1c5a146350f7c7c0cde6c08ac477ce74b46
Pierre Genthon <genthon.pierre@gmail.com>
2026-02-03 13:53:37 +0100

    add cache to build front

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,13 +1,13 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-const uri = process.env.NEXT_PUBLIC_GRAPHQL_API_URL;
+const uri = process.env.NEXT_PUBLIC_GRAPHQL_API_URL || "/graphql";
 
 console.log({ uri });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: uri || "/graphql",
+    uri,
     credentials: "include", // Important: Include cookies with requests
   }),
 });
