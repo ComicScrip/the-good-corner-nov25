1e9bb31ac4ca60c7baa49bdb2c4ca376308a3e68
Pierre Genthon <genthon.pierre@gmail.com>
2026-03-11 12:02:40 +0100

    feat: add S3 backup configuration and restore functionality with rclone

---
+++ restore_backup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+export $(grep -v '^#' .env.production | xargs)
+
+function choose_from_menu() {
+    local prompt="$1" outvar="$2"
+    shift
+    shift
+    local options=("$@") cur=0 count=${#options[@]} index=0
+    local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
+    printf "$prompt\n"
+    while true
+    do
+        # list all options (option list is zero-based)
+        index=0 
+        for o in "${options[@]}"
+        do
+            if [ "$index" == "$cur" ]
+            then echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
+            else echo "  $o"
+            fi
+            index=$(( $index + 1 ))
+        done
+        read -s -n3 key # wait for user to key in arrows or ENTER
+        if [[ $key == $esc[A ]] # up arrow
+        then cur=$(( $cur - 1 ))
+            [ "$cur" -lt 0 ] && cur=0
+        elif [[ $key == $esc[B ]] # down arrow
+        then cur=$(( $cur + 1 ))
+            [ "$cur" -ge $count ] && cur=$(( $count - 1 ))
+        elif [[ $key == "" ]] # nothing, i.e the read delimiter - ENTER
+        then break
+        fi
+        echo -en "\e[${count}A" # go up to the beginning to re-render
+    done
+    # export the selection to the requested output variable
+    printf -v $outvar "${options[$cur]}"
+}
+
+DB_CONTAINER_NAME="prod-db"
+BACKUPS_FOLDER=".backups"
+CURRENT_BACKUP_FOLDER=""
+RCLONE_REMOTE_NAME="s3"
+RCLONE_REMOTE_FOLDER="the-good-corner-nov25"
+
+echo "syncing from remote to local..."
+rclone sync $RCLONE_REMOTE_NAME:$RCLONE_REMOTE_FOLDER $BACKUPS_FOLDER
+
+
+BACKUPS=($(ls $BACKUPS_FOLDER | sort -r))
+
+if [ ${#BACKUPS[@]} -eq 0 ]; then
+    echo "No backups available..."
+    exit 1
+fi
+
+choose_from_menu "Please choose which backup to restore from :" CURRENT_BACKUP_FOLDER "${BACKUPS[@]}"
+echo "Restoring from $CURRENT_BACKUP_FOLDER backup folder..."
+
+FOLDER="$BACKUPS_FOLDER/$CURRENT_BACKUP_FOLDER"
+
+echo "Restoring DB..."
+cat $FOLDER/dump.sql | docker exec -e PGPASSWORD=$DB_PASS -i $DB_CONTAINER_NAME psql -U $DB_USER -d postgres
+
+echo "Purging kvStore cache..."
+docker exec -i prod-kvStore redis-cli FLUSHALL
+
+echo "done !"
